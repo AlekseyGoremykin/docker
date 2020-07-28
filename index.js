f8498d16ae504ced21d02eb34f9ddcad362e8d74
Alexey Goremykin <alexgaga1996@gmail.com>
2020-07-28 09:50:28 +0300

    add router methods

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const Koa = require ('koa');
 const Router= require('koa-router');
+const bodyparser = require('koa-bodyparser');
+
 
 const db = require('@paralect/node-mongo').connect('mongodb://db:27017/docker');
 const app = new Koa();
 const router = new Router();
-const service = db.createService('users');
+const usersService = db.createService('users');
+const logsService = db.createService('logs');
+
+app.use(bodyparser());
 
 router.get('/me', async (ctx, next) => {
-  next();
+  ctx.body = await usersService.find({})
 })
 
 router.post('/logs', async (ctx, next) => {
-  console.log(ctx.request.body)
+  logsService.create(ctx.request.body);
   next();
 })
 
 router.get('/logs', async (ctx, next) => {
-  next();
+  ctx.body = await logsService.find({})
 })
 
 
